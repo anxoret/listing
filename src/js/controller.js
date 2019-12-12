2726901d6788e0d47426d18f86d15ae2b840e9f9
anxoret <post.lux@yandex.ru>
2019-12-12 22:45:16 +0400

    New basic functions

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,25 @@
 "use strict"
+
+// controller
+const showPreviousImage = () => {
+
+};
+
+const showNextImage = () => {
+
+};
+
+// model
+
+
+// view
+const showImage = () => {
+    
+};
+
+// main
+let slideshowLeftButton = document.querySelector(".slideshow__button_left"); 
+
+slideshowLeftButton.addEventListener("click", () => {
+    showPreviousImage();
+});
