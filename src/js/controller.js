758e6b8a925f590481c37240eed921876a162f28
anxoret <post.lux@yandex.ru>
2019-12-15 21:56:54 +0400

    Made slideshow working right, but without errors checking

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,11 @@
 "use strict"
 
 // обработать ошибки при вызове функции контроллера, к. создает слайдшоу
-// сделать дозированность изображений в слайдшоу
 
 // view => slideshow.js
 let vCurrentShownImageNumber;
 let vSlideshowLength;
+let slideshowImg;
 let slideshowButtonRight;
 let slideshowButtonLeft;
 
@@ -36,7 +36,7 @@ const vCreateSlideshow = (arrayOfImages, shownImageNumber) => {
     slideshowContainer.append(slideshowButtonLeft);
     
     let leftButtonImg = document.createElement("img");
-    leftButtonImg.className = "slideshow__img slideshow__img_theme_original";
+    leftButtonImg.className = "slideshow__arrow-img slideshow__arrow-img_theme_original";
     leftButtonImg.setAttribute("src", "./img/arrow-left.png");
     leftButtonImg.setAttribute("alt", "arrow-left");
     slideshowButtonLeft.append(leftButtonImg);
@@ -46,53 +46,53 @@ const vCreateSlideshow = (arrayOfImages, shownImageNumber) => {
     slideshowContainer.append(slideshowButtonRight);
 
     let rightButtonImg = document.createElement("img");
-    rightButtonImg.className = "slideshow__img slideshow__img_theme_original";
+    rightButtonImg.className = "slideshow__arrow-img slideshow__arrow-img_theme_original";
     rightButtonImg.setAttribute("src", "./img/arrow-left.png");
     rightButtonImg.setAttribute("alt", "arrow-right");
     slideshowButtonRight.append(rightButtonImg);
 
-    arrayOfImages.forEach( (image, imageNumber) => {
-        if (imageNumber == 0) {
-            makeButtonInactive(slideshowButtonLeft);
-        }
+    if (shownImageNumber == 0) {
+        makeButtonInactive(slideshowButtonLeft);
+    }
 
-        let slideshowImgContainer = document.createElement("div");
+    let slideshowImgContainer = document.createElement("div");
+    slideshowImgContainer.className = "slideshow__img-container_theme_original slideshow__img_show";
 
-        if (imageNumber == shownImageNumber) {
-            slideshowImgContainer.className = "slideshow__img-container slideshow__img-container_theme_original slideshow__img-container_show"
-        } else {
-            slideshowImgContainer.className = "slideshow__img-container slideshow__img-container_theme_original";
-        }
+    slideshowContainer.append(slideshowImgContainer);
 
-        slideshowContainer.append(slideshowImgContainer);
+    slideshowImg = document.createElement("img");
+    slideshowImg.className = "slideshow__img slideshow__img_theme_original slideshow__img_show";
 
-        let slideshowImg = document.createElement("img");
-        slideshowImg.className = "slideshow__img slideshow__img_theme_original";
-
-        for (let attributeName in image) {
-            slideshowImg.setAttribute(attributeName, image[attributeName]);
-        }
-        
-        slideshowImgContainer.append(slideshowImg); 
-    });
+    for (let attributeName in arrayOfImages[shownImageNumber]) {
+        slideshowImg.setAttribute(attributeName, arrayOfImages[shownImageNumber][attributeName]);
+    }
+    
+    slideshowImgContainer.append(slideshowImg); 
 };
 
-const vShowSlideshowImage = (imageToShow) => {
-    let currentShownImageContainer = document.querySelector(".slideshow__img-container_show");
-    currentShownImageContainer.classList.remove("slideshow__img-container_show");
+const vChangeImgSrcAndAlt = (imageToShow) => {
+    slideshowImg.classList.remove("slideshow__img_show");
+    setTimeout( () => {
+        slideshowImg.setAttribute("src", imageToShow["src"]);
+        slideshowImg.setAttribute("alt", imageToShow["alt"]);
+        slideshowImg.classList.add("slideshow__img_show");
+    }, 500);
 
-    let imageContainerToShow = document.querySelectorAll(".slideshow__img-container")[imageToShow];
-    imageContainerToShow.classList.add("slideshow__img-container_show");
+};
 
-    vCurrentShownImageNumber = imageToShow;
+const vShowSlideshowImage = (imageToShowNumber) => {
+    let imageToShow = cGetSlideshowShownImage();
+    vChangeImgSrcAndAlt(imageToShow);
 
-    if (imageToShow == 0) {
+    vCurrentShownImageNumber = imageToShowNumber;
+
+    if (imageToShowNumber == 0) {
         makeButtonInactive(slideshowButtonLeft);
     } else {
         makeButtonActive(slideshowButtonLeft);
     }
 
-    if (imageToShow == vSlideshowLength - 1) {
+    if (imageToShowNumber == vSlideshowLength - 1) {
         makeButtonInactive(slideshowButtonRight);
     } else {
         makeButtonActive(slideshowButtonRight);
@@ -129,7 +129,7 @@ setTimeout( () => vHangClicksOnSlideshowButtons());
 const cCreateSlideshow = (arrayOfImages, shownImage) => {
     mCreateSlideshow(arrayOfImages, shownImage);
     vCreateSlideshow(
-        mGetSlideshow(), mGetSlideshowShownImage()
+        mGetSlideshow(), mGetSlideshowShownImageNumber()
     );
 };
 
@@ -137,24 +137,29 @@ const cGetSlideshowLength = () => {
     return mGetSlideshowLength();
 };
 
+const cGetSlideshowShownImage = () => {
+    return mGetSlideshowShownImage();
+};
+
 const cShowPreviousSlideshowImage = () => {
     mShowPreviousSlideshowImage();
-    let newShownImage = mSlideshow.getShownImage();
+    let newShownImage = mSlideshow.getShownImageNumber();
     vShowSlideshowImage(newShownImage);                                     
 };
 
 const cShowNextSlideshowImage = () => {
     mShowNextSlideshowImage();
-    let newShownImage = mSlideshow.getShownImage();
+    let newShownImage = mSlideshow.getShownImageNumber();
     vShowSlideshowImage(newShownImage);      
 };
 
 // model => slideshow.js 
 class MImages {
-    constructor (array, shownImage) {
+    constructor (array, shownImageNumber) {
         this.array = array;
-        this.shownImage = shownImage - 1;
-        // обработать эти 2 поля!!! ^
+        this.shownImage = array[shownImageNumber];
+        this.shownImageNumber = shownImageNumber - 1;
+        // обработать эти поля!!! ^
     }
 
     setImageArray(array) {
@@ -162,13 +167,18 @@ class MImages {
     }
 
     setShownImage(imageNumberInArray) {
-        if (this.shownImage == imageNumberInArray) return;
+        if (this.shownImageNumber == imageNumberInArray) return;
 
-        this.shownImage = imageNumberInArray;
+        this.shownImage = this.array[imageNumberInArray];
+        this.shownImageNumber = imageNumberInArray;
     }
 
     getImagesArray() {
         return this.array;
+    }
+
+    getShownImageNumber() {
+        return this.shownImageNumber;
     }
 
     getShownImage() {
@@ -186,48 +196,51 @@ const mGetSlideshow = () => {
     return mSlideshow.getImagesArray();
 };
 
+const mGetSlideshowShownImageNumber = () => {
+    return mSlideshow.getShownImageNumber();
+};
+
 const mGetSlideshowShownImage = () => {
     return mSlideshow.getShownImage();
 };
 
-const mChangeSlideshowImage = (imageToShow) => {
-    mSlideshow.setShownImage(imageToShow);
+const mChangeSlideshowImage = (imageToShowNumber) => {
+    mSlideshow.setShownImage(imageToShowNumber);
 };
 
 const mShowPreviousSlideshowImage = () => {
-    let currentShownImage = mSlideshow.getShownImage();
-    let imageToShow = currentShownImage - 1;
-    mChangeSlideshowImage(imageToShow);
+    let currentShownImage = mSlideshow.getShownImageNumber();
+    let imageToShowNumber = currentShownImage - 1;
+    mChangeSlideshowImage(imageToShowNumber);
 };
 
 const mShowNextSlideshowImage = () => {
-    let currentShownImage = mSlideshow.getShownImage();
-    let imageToShow = currentShownImage + 1;
-    mChangeSlideshowImage(imageToShow);
+    let currentShownImage = mSlideshow.getShownImageNumber();
+    let imageToShowNumber = currentShownImage + 1;
+    mChangeSlideshowImage(imageToShowNumber);
 };
 
 // slideshow.js => main.js
 let arrayOfImages = [
-    // {src: "./img/banner-0.png", alt: "banner-0"},
     {src: "./img/banner-1.png", alt: "banner-1"},
     {src: "./img/banner-2.png", alt: "banner-2"},
-    {src: "./img/banner-3.png", alt: "banner-3"}
-    // {src: "./img/banner-0.png", alt: "banner-4"},
-    // {src: "./img/banner-1.png", alt: "banner-5"},
-    // {src: "./img/banner-2.png", alt: "banner-6"},
-    // {src: "./img/banner-3.png", alt: "banner-7"},
-    // {src: "./img/banner-0.png", alt: "banner-8"},
-    // {src: "./img/banner-1.png", alt: "banner-9"},
-    // {src: "./img/banner-0.png", alt: "banner-10"},
-    // {src: "./img/banner-1.png", alt: "banner-11"},
-    // {src: "./img/banner-2.png", alt: "banner-12"},
-    // {src: "./img/banner-3.png", alt: "banner-13"},
-    // {src: "./img/banner-0.png", alt: "banner-14"},
-    // {src: "./img/banner-1.png", alt: "banner-15"},
-    // {src: "./img/banner-2.png", alt: "banner-16"},
-    // {src: "./img/banner-3.png", alt: "banner-17"},
-    // {src: "./img/banner-0.png", alt: "banner-18"},
-    // {src: "./img/banner-1.png", alt: "banner-19"},
+    {src: "./img/banner-3.png", alt: "banner-3"},
+    {src: "./img/banner-0.png", alt: "banner-4"},
+    {src: "./img/banner-1.png", alt: "banner-5"},
+    {src: "./img/banner-2.png", alt: "banner-6"},
+    {src: "./img/banner-3.png", alt: "banner-7"},
+    {src: "./img/banner-0.png", alt: "banner-8"},
+    {src: "./img/banner-1.png", alt: "banner-9"},
+    {src: "./img/banner-0.png", alt: "banner-10"},
+    {src: "./img/banner-1.png", alt: "banner-11"},
+    {src: "./img/banner-2.png", alt: "banner-12"},
+    {src: "./img/banner-3.png", alt: "banner-13"},
+    {src: "./img/banner-0.png", alt: "banner-14"},
+    {src: "./img/banner-1.png", alt: "banner-15"},
+    {src: "./img/banner-2.png", alt: "banner-16"},
+    {src: "./img/banner-3.png", alt: "banner-17"},
+    {src: "./img/banner-0.png", alt: "banner-18"},
+    {src: "./img/banner-1.png", alt: "banner-19"},
 ];
 
 let slideshowImages = cCreateSlideshow(arrayOfImages, 1);
